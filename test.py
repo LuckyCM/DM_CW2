5ba76e722a73ac76d6032ed9be2f1124d4009579
ChenMin <670501882@qq.com>
2021-03-27 09:26:06 +0800

    DataMing_Coursework2 delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from sklearn.feature_extraction.text import CountVectorizer
-import numpy as np
-texts=["dog cat fish","dog cat cat","fish bird", 'bird'] # “dog cat fish” 为输入列表元素,即代表一个文章的字符串
-texts = np.array(texts)
-cv = CountVectorizer()#创建词袋数据结构
-cv_fit=cv.fit_transform(texts)
-print(cv_fit.toarray())
